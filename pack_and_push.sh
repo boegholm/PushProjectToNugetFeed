b576e842b2d7e7704b182d66c1addbaa9a8a4c62
Thomas Bøgholm <thomas@boegholm.dk>
2022-02-21 22:52:47 +0100

    array of projects

---
+++ pack_and_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+projectjson="$1"
+version="$2"
+NUGET_KEY="$3"
+NUGET_SOURCE"$4"
+NUGET_SYM_SOURCE="$5"
+
+echo "$projectjson" | jq '.[]' | while read project; do
+      dotnet pack "$project"/"$project".csproj --include-source --include-symbols -p:PackageVersion="$inputs" --output nupkgs -p:SymbolPackageFormat=snupkg
+      dotnet nuget push ./nupkgs/*.nupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SOURCE
+      dotnet nuget push ./nupkgs/*.snupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SYM_SOURCE
+done
