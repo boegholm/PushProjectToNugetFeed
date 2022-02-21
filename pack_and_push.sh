0cef5a804e27fe61877ca1bcc9a5e33320e80cca
Thomas Bøgholm <thomas@boegholm.dk>
2022-02-21 23:10:06 +0100

    fix

--- pack_and_push.sh
+++ pack_and_push.sh
@@ -10,4 +10,5 @@ echo "$projectjson" | jq '.[]' | while read project; do
       dotnet pack "$project" --include-source --include-symbols -p:PackageVersion="$inputs" --output nupkgs -p:SymbolPackageFormat=snupkg
       dotnet nuget push ./nupkgs/*.nupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SOURCE
       dotnet nuget push ./nupkgs/*.snupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SYM_SOURCE
+      rm ./nupkgs/*.nupkg ./nupkgs/*.snupkg
 done
