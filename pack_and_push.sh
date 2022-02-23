4d6d5d98dc07a0d4fb3010b74afaab3325ccb1fa
Thomas Bøgholm <thomas@boegholm.dk>
2022-02-23 04:52:44 +0100

    fixed json for jq

--- pack_and_push.sh
+++ pack_and_push.sh
@@ -6,7 +6,7 @@ NUGET_KEY="$3"
 NUGET_SOURCE="$4"
 NUGET_SYM_SOURCE="$5"
 
-echo "$projectjson" | jq -r '.[]' | while read project; do
+echo "$projectjson" |sed "s/'/\"/g" | jq -r '.[]' | while read project; do
       dotnet pack "$project" --include-source --include-symbols -p:PackageVersion="$inputs" --output nupkgs -p:SymbolPackageFormat=snupkg
       dotnet nuget push ./nupkgs/*.nupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SOURCE
       dotnet nuget push ./nupkgs/*.snupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SYM_SOURCE
