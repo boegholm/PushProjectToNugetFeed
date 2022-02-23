5f2ccd19fc409b7a8352c75fdba98df018f02040
Thomas Bøgholm <thomas@boegholm.dk>
2022-02-23 05:09:27 +0100

    more verbose

--- pack_and_push.sh
+++ pack_and_push.sh
@@ -5,11 +5,15 @@ version="$2"
 NUGET_KEY="$3"
 NUGET_SOURCE="$4"
 NUGET_SYM_SOURCE="$5"
-
+echo Packing projects: "$projectjson"
 echo "$projectjson" |sed "s/'/\"/g" | jq -r '.[]' | while read project; do
       echo "Pack project: $project"
       dotnet pack "$project" --include-source --include-symbols -p:PackageVersion="$inputs" --output nupkgs -p:SymbolPackageFormat=snupkg
+      echo pushing
       dotnet nuget push ./nupkgs/*.nupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SOURCE
       dotnet nuget push ./nupkgs/*.snupkg --skip-duplicate -k $NUGET_KEY -s $NUGET_SYM_SOURCE
+      echo removing pushed"
       rm ./nupkgs/*.nupkg ./nupkgs/*.snupkg
+      echo removed, done
 done
+echo "done packing"
